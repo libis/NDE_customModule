9e3f5975395c53390fe0e628f40044a94663d7c1
Lydia Janssen <lydia.janssen@kuleuven.be>
2026-04-10 09:32:05 +0200

    component mappings + json read support

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,6 +1,9 @@
+import { LibisPasswordNoteComponent } from "./libis-password-note/libis-password-note.component";
+import { LibisPermalinkComponent } from "./libis-permalink/libis-permalink.component";
+
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
 
-
-
+['nde-permalink-dialog-after', LibisPermalinkComponent],
+['nde-view-it-card-bottom', LibisPasswordNoteComponent]
 ]);
