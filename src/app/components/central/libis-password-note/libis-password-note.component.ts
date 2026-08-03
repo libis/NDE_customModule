6fd26c61f3fbe8a0bdaced97c655841be1551938
Lydia Janssen <lydia.janssen@kuleuven.be>
2026-08-03 18:22:26 +0200

    Cleaned up version icon-source + password-note

--- src/app/components/central/libis-password-note/libis-password-note.component.ts
+++ src/app/components/central/libis-password-note/libis-password-note.component.ts
@@ -25,6 +25,7 @@ export class LibisPasswordNoteComponent {
   private electronicService!: ElectronicService;
   private recID: Signal<string|undefined> = this.store.selectSignal(selectFullDisplayRecordId);
   credentials: string = '';
+  isLoading: boolean = false;
   private loggedIn = this.userState.isLoggedInSignal();
   private userJWT = this.userState.jwtSignal();
   private inst = this.viewConfigState.institutionCodeSignal();
@@ -36,6 +37,8 @@ export class LibisPasswordNoteComponent {
     }
     return false;
 } );
+
+
   //accessInfo: string|undefined = undefined;
 
   constructor(
@@ -58,6 +61,7 @@ export class LibisPasswordNoteComponent {
 
   // Collect credentials - triggered by explicit button click to maximally protect credentials
   public collectCredentials(): void {
+    this.isLoading = true;
     console.log('Collecting credentials for view it card:', this.hostComponent);
       const portfolioId = this.electronicService.ilsApiId;
       console.log('Portfolio ID:', portfolioId);
@@ -79,14 +83,13 @@ export class LibisPasswordNoteComponent {
 
       const headers = new HttpHeaders({
       Authorization: `Bearer ${userJWT}`
-      //Authorization: 'Bearer eyJraWQiOiJwcmltYVByaXZhdGVLZXktMzJLVUxfS1VMIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJQcmltYSIsImp0aSI6IkFDRjcwNDgxRjY3ODZDMDUxMDdFRDNDM0FCNjk3OEU2LmFwcDAxLmV1MDAucHNiLmFsbWEuZGMwNi5ob3N0ZWQuZXhsaWJyaXNncm91cC5jb206MTgwMSIsImV4cCI6MTc4MTI0NzI2NiwiaWF0IjoxNzgxMTYwODY2LCJ1c2VyTmFtZSI6InRhMDEiLCJkaXNwbGF5TmFtZSI6IlRlc3RlcjAxLCBBbG1hIiwidXNlciI6IjIyMTEwOTMwNDkzMjAwMDE0ODgiLCJ1c2VyR3JvdXAiOiIxNCIsImluc3RpdHV0aW9uIjoiMzJLVUxfS1VMIiwidXNlcklwIjoiNS4yMy4xNDQuMTAxIiwiYXV0aGVudGljYXRpb25Qcm9maWxlIjoiQWxtYSIsImF1dGhlbnRpY2F0aW9uU3lzdGVtIjoiIiwibGFuZ3VhZ2UiOiJlbiIsInNhbWxTZXNzaW9uSW5kZXgiOiIiLCJzYW1sTmFtZUlkIjoiIiwib25DYW1wdXMiOiJmYWxzZSIsInNpZ25lZEluIjoidHJ1ZSIsInZpZXdJZCI6IjMyS1VMX0tVTDpLVUxldXZlbl9OREUiLCJzZWxmUmVnaXN0ZXJlZCI6ImZhbHNlIiwicmVzdHJpY3RlZFVzZXIiOiJmYWxzZSJ9.eCjiRQY9Avy0JyV7M2eQm6t8EXH8p_aYr6PRfbSXsz9kIpVu6U7l_2Ee1HZqnxdrolPJ-nglDusiV6HqU3nIEQ'
-      //Authorization: `Bearer eyJraWQiOiJwcmltYVByaXZhdGVLZXktMzJLVUxfS1VMIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJQcmltYSIsImp0aSI6IjY0NTVENUUyRDMzRTFDMDYwMDhEQkQwRkVFMEY3QzcwLmFwaTAyLmV1MDAucHJvZC5hbG1hLmRjMDMuaG9zdGVkLmV4bGlicmlzZ3JvdXAuY29tOjE4MDEiLCJleHAiOjE3ODEyNTM2OTcsImlhdCI6MTc4MTE2NzI5NywidXNlck5hbWUiOiJ0YTAxIiwiZGlzcGxheU5hbWUiOiJ0YTAxIiwidXNlciI6IjIyMTEwOTMwNDkzMjAwMDE0ODgiLCJ1c2VyR3JvdXAiOiIxNCIsImluc3RpdHV0aW9uIjoiMzJLVUxfS1VMIiwidXNlcklwIjoiMTAuMTYuMTIwLjY5IiwiYXV0aGVudGljYXRpb25Qcm9maWxlIjoiIiwiYXV0aGVudGljYXRpb25TeXN0ZW0iOiIiLCJsYW5ndWFnZSI6ImVuIiwic2FtbFNlc3Npb25JbmRleCI6IiIsInNhbWxOYW1lSWQiOiIiLCJvbkNhbXB1cyI6InRydWUiLCJzaWduZWRJbiI6InRydWUiLCJ2aWV3SWQiOiIzMktVTF9LVUw6S1VMZXV2ZW5fTkRFIiwic2VsZlJlZ2lzdGVyZWQiOiJmYWxzZSIsInJlc3RyaWN0ZWRVc2VyIjoiZmFsc2UifQ.WxEK7DUzYqLdCny3wH4pWJTDtF9yKwTzBCrAmA-asu26C5PhndLYezAPY7hWhb-UbMU1l9idkX4cMj5WUrD3Bw`,
     });
     console.log('Constructed HTTP headers:', headers.toString());
 
       this.http.get(
-        //'https://eu-workflows.hosted.exlibrisgroup.com/PSB-19868343-9f49-454d-b9b5-84e5dba9923f/webhook-test/e8df7a08-a017-4ecb-84b3-0c94e4dc2767',
+        //LibOW Password Note link
         'https://eu-workflows.hosted.exlibrisgroup.com/PSB-19868343-9f49-454d-b9b5-84e5dba9923f/webhook/e8df7a08-a017-4ecb-84b3-0c94e4dc2767',
+        //'https://eu-workflows.hosted.exlibrisgroup.com/19868343-9f49-454d-b9b5-84e5dba9923f/webhook/e8df7a08-a017-4ecb-84b3-0c94e4dc2767',
         {
           params: params,
           headers: headers
@@ -96,9 +99,15 @@ export class LibisPasswordNoteComponent {
           console.log('Credentials response:', response.credentials);
           console.log('Response type:', typeof response);
           this.credentials = response['credentials'];
+          this.isLoading = false;
         },
-        error: err => console.error('Error fetching credentials:', err),
-        complete: () => console.log('Credential request completed')
+        error: err => {
+          console.error('Error fetching credentials:', err);
+          this.isLoading = false;},
+        complete: () => {
+          console.log('Credential request completed')
+          this.isLoading = false;
+                }
   });
       } else {
         console.error('MMS ID not found in authNote:', this.electronicService.authNote);
